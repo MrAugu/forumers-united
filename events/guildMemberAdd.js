0ee26c4e1b7b9f9a9dd17c6caa544b45eb83ce8b
MrAugu <39545629+MrAugu@users.noreply.github.com>
2020-01-04 22:13:46 +0200

    Message Members on Join

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,8 +6,10 @@ module.exports = class {
   }
 
   async run (member) {
-    //member.user.send(`Hello **${member.user.username}**!\nThank you for joining us in Forumers United server. There are a few more steps into gaining acess. You can post your discord tag at https://www.growtopiagame.com/forums/showthread.php?550527-Forumers-United, however is not neccesary.\n\nType \`+verify\` in here, the same channel in which you are reading this at moment, you'll be asked your forum name and to confirm it, then one of admins will handle your request.`).catch(e => {});
-
-    //this.client.channels.get("595969433059721219").send(`<:rolleyes:597417024704086016> **${member.user.tag}** has joined the server! There are now **${member.guild.memberCount}** peoples in the server!`)
+    const embed  = new Discord.MessageEmbed()
+      .setDescription(`Hello ${member.user.username}!\nWelcome to Forumers United Discord Server, for more information please check the #verification channel.`)
+      .setColor("#36393e")
+      .setTimestamp();
+    member.user.send(embed).catch(e => {});
   }
 };
